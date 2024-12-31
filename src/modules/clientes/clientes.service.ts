56e28baa183c79fcc16044257325305afb9f0b34
Josue Guardado <jguardadosv@gmail.com>
2024-12-31 17:24:27 -0600

    antes de update

--- src/modules/clientes/clientes.service.ts
+++ src/modules/clientes/clientes.service.ts
@@ -1,5 +1,9 @@
 // cliente.service.ts
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { cliente as Cliente, Prisma } from '@prisma/client';
 
@@ -7,7 +11,8 @@ import { cliente as Cliente, Prisma } from '@prisma/client';
 export class ClientesService {
   constructor(private prisma: PrismaService) {}
 
-  async cliente(
+  /*
+   async cliente(
     clienteWhereUniqueInput: Prisma.clienteWhereUniqueInput,
   ): Promise<Cliente | null> {
     return this.prisma.cliente.findUnique({
@@ -16,6 +21,44 @@ export class ClientesService {
         empresa: true,
       },
     });
+  }*/
+  async cliente(
+    clienteWhereUniqueInput: Prisma.clienteWhereUniqueInput,
+  ): Promise<Cliente | null> {
+    try {
+      const cliente = await this.prisma.cliente.findUnique({
+        where: clienteWhereUniqueInput,
+        include: {
+          empresa: true,
+        },
+      });
+
+      // Si no se encuentra el cliente, lanzamos una excepción NotFound
+      if (!cliente) {
+        throw new NotFoundException({
+          statusCode: 404,
+          message: 'Cliente no encontrado con los datos proporcionados.',
+        });
+      }
+
+      return cliente;
+    } catch (error) {
+      // Manejo de errores específicos de Prisma
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        // Puedes manejar diferentes códigos de error de Prisma aquí
+        if (error.code === 'P2025') {
+          throw new NotFoundException({
+            statusCode: 404,
+            message: 'Cliente no encontrado con los datos proporcionados.',
+          });
+        }
+      }
+
+      // Si no es un error conocido, lanzamos una excepción genérica
+      throw new InternalServerErrorException(
+        'Ocurrió un error al obtener el cliente.',
+      );
+    }
   }
 
   async clientes(params: {
